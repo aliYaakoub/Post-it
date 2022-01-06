d464cfa01a6747b73d166a43951f083e2b1af00a
ali yaakoub <ali.yaakoub.2021@gmail.com>
2022-01-07 00:34:28 +0200

    bug fix

--- src/component/ProgressBar.jsx
+++ src/component/ProgressBar.jsx
@@ -1,18 +1,21 @@
 import React, { useEffect } from 'react'
 import useStorage from '../hooks/useStorage'
 import { motion } from 'framer-motion';
+import { useAuth } from './../contexts/AuthContext';
 
 const ProgressBar = ({file, setFile, setFileToUpload, path, posterId, content=false, setNewPost, type=false, userId = false}) => {
 
     const { url, progress } = useStorage(file, path, posterId, content, type, userId);
+    const { setChanges } = useAuth();
     
     useEffect(()=>{
         if(url){
             setFile(null);
             setFileToUpload(null);
             setNewPost(false);
+            setChanges(old => old + 1)
         }
-    },[url, setFile, setFileToUpload, setNewPost])
+    },[url, setFile, setFileToUpload, setNewPost, setChanges])
 
     return (
         <motion.div 
